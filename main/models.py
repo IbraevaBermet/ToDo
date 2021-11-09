1e60e2dd7699a5cb02721f5a4694c7ac4587fc39
IbraevaBermet <ibraevabermet093@mail.ru>
2021-11-09 23:23:27 +0600

    add models to admin list

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number =models.IntegerField(max_length=15) 
+    date_of_meeting= models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+
+
+        
