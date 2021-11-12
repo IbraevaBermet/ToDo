62419541725496b7f314c08a0dd7b40dfe2adbbb
IbraevaBermet <ibraevabermet093@mail.ru>
2021-11-12 23:49:23 +0600

    added todo marked

--- todo/urls.py
+++ todo/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path("habits/",habits,name="habits"),
     path("add-habits/",add_habits,name="add_habits"),
     path("delete-todo/<id>/",delete_todo,name="delete-todo"),
+    path("mark-todo/<id>/",mark_todo,name="mark-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
