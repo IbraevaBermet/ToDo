7292fca0918b470490d84cf3d428c30230575158
IbraevaBermet <ibraevabermet093@mail.ru>
2021-11-12 18:34:29 +0600

    added todo deletion

--- main/views.py
+++ main/views.py
@@ -49,6 +49,12 @@ def add_habits(request):
     habits.save()
     return redirect(habits)
    
+def delete_todo(request,id):
+    todo= ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+
 
 
 
--- todo/urls.py
+++ todo/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path("add-meet/",add_meet,name="add-meet"),
     path("habits/",habits,name="habits"),
     path("add-habits/",add_habits,name="add_habits"),
+    path("delete-todo/<id>/",delete_todo,name="delete-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
